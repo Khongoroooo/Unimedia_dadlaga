4a617f0b6195159ea30eeaeefa3a88fa619f9e90
Svhe4u <hongoroo1289810@gmail.com>
2025-05-27 00:41:28 +0800

    part5

--- part5.js
+++ part5.js
@@ -6,3 +6,19 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(2, 5));
+var num = function (num1) { return num1 * 2; };
+console.log(num(2));
+var numm = function (num1) {
+    return num1 * 2;
+};
+console.log(numm(3));
+function getnumbers(numbers) {
+    return numbers.filter(function (num) { return num % 2 === 0; });
+}
+var numberee = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var numbere = getnumbers(numberee);
+console.log(numbere);
+var getnumberss = function (numberss) { return numberss.filter(function (num) { return num % 2 === 0; }); };
+var simple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var last = getnumberss(simple);
+console.log(last);
--- part5.ts
+++ part5.ts
@@ -2,6 +2,7 @@ function greet(name: string): string {
   return `hello, ${name}`;
 }
 
+//2 toonii niilber
 console.log(greet("khongoroo"));
 
 function add(num1: number, num2: number): number {
@@ -9,4 +10,28 @@ function add(num1: number, num2: number): number {
 }
 
 console.log(add(2, 5));
-function
+
+//sumtai function ashiglah bogino bicheglel
+const num = (num1: number): number => num1 * 2;
+console.log(num(2));
+
+//urt ni
+const numm = (num1: number): number => {
+  return num1 * 2;
+};
+console.log(numm(3));
+
+//array aas tegsh toonuudiig gargah
+function getnumbers(numbers: number[]): number[] {
+  return numbers.filter((num) => num % 2 === 0);
+}
+const numberee = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbere = getnumbers(numberee);
+console.log(numbere);
+
+//sumtai function
+const getnumberss = (numberss: number[]): number[] =>
+  numberss.filter((num) => num % 2 === 0);
+const simple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const last = getnumberss(simple);
+console.log(last);
